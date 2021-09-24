2befb5800cbc6102921a177d85c38e9d129a4177
Oguzhan Dogdu <oguzd494@gmail.com>
2021-09-25 01:29:37 +0300

    Finish app!

--- app/src/main/java/com/oguzdogdu/jetpackcompose_countrylist/MainActivity.kt
+++ app/src/main/java/com/oguzdogdu/jetpackcompose_countrylist/MainActivity.kt
@@ -1,23 +1,50 @@
 package com.oguzdogdu.jetpackcompose_countrylist
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.oguzdogdu.jetpackcompose_countrylist.ui.theme.JetpackComposeCountryListTheme
+import com.oguzdogdu.jetpackcompose_countrylist.ui.theme.Purple500
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeCountryListTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Scaffold(
+                        topBar = { TopBar() },
+                        backgroundColor = Purple500
+                    ) {
+                        CountryNavigation()
+                    }
                 }
             }
         }
@@ -25,14 +52,165 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Country List",
+                fontSize = 20.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        },
+        backgroundColor = Purple500,
+        contentColor = Color.White
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeCountryListTheme {
-        Greeting("Android")
+fun CountryNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "CountryList"
+    ) {
+        composable("CountryList") {
+            CountryListScreen()
+        }
     }
+}
+
+@Composable
+fun CountryListScreen() {
+    val textVal = remember { mutableStateOf(TextFieldValue("")) }
+    Column {
+        SearchCountryList(textVal)
+        CountryList(textVal)
+    }
+}
+
+@Composable
+fun SearchCountryList(textVal: MutableState<TextFieldValue>) {
+    TextField(
+        value = textVal.value,
+        onValueChange = { textVal.value = it },
+        placeholder = { Text(text = "Search Country Name") },
+        modifier = Modifier
+            .fillMaxWidth(),
+        textStyle = TextStyle(Color.Black, fontSize = 18.sp),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "Search",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (textVal.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        textVal.value = TextFieldValue("")
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RectangleShape,
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.Black,
+            cursorColor = Color.Black,
+            leadingIconColor = Color.Black,
+            trailingIconColor = Color.Black,
+            backgroundColor = Color.White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+fun CountryList(textVal: MutableState<TextFieldValue>) {
+    val context = LocalContext.current
+    val countries = getListOfCountries()
+    var filteredCountries: ArrayList<String>
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val searchText = textVal.value.text
+        filteredCountries = if (searchText.isEmpty()) {
+            countries
+        } else ({
+            val resultList = mutableListOf<String>()
+            for (country in countries) {
+                if (country.lowercase(java.util.Locale.getDefault())
+                        .contains(searchText.lowercase(java.util.Locale.getDefault()))
+                ) {
+                    resultList.add(country)
+                }
+            }
+            resultList
+        }) as ArrayList<String>
+        items(filteredCountries) { filteredCountries ->
+            CountryListItem(
+                countryText = filteredCountries,
+                onItemClick = { selectedCountry ->
+                    Toast.makeText(context, selectedCountry, Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun CountryListItem(
+    countryText: String,
+    onItemClick: (String) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .clickable {
+                onItemClick(countryText)
+            }
+            .background(Color.White)
+            .height(60.dp)
+            .fillMaxWidth()
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = countryText,
+            fontSize = 16.sp,
+            color = Color.Black,
+            modifier = Modifier.padding(start = 10.dp)
+        )
+    }
+}
+
+@Composable
+fun getListOfCountries(): ArrayList<String> {
+    val isoCountryCodes = java.util.Locale.getISOCountries()
+    val countryListWithEmojis = ArrayList<String>()
+    for (countryCode in isoCountryCodes) {
+        val locale = java.util.Locale("", countryCode)
+        val countryName = locale.displayCountry
+        val flagOffset = 0x1F1E6
+        val asciiOffset = 0x41
+        val firstChar = Character.codePointAt(countryCode, 0) - asciiOffset + flagOffset
+        val secondChar = Character.codePointAt(countryCode, 1) - asciiOffset + flagOffset
+        val flag = (String(Character.toChars(firstChar)) + String(Character.toChars(secondChar)))
+        countryListWithEmojis.add("$countryName (${locale.country}) $flag")
+    }
+    return countryListWithEmojis
 }
